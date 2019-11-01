a159a7212dd007076c12af05770f514ba0de9f06
Ravshan <ravkart@gmail.com>
2019-11-01 14:25:40 -0700

    remove redux logs

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,7 +15,7 @@ const middlewares = [sagaMiddleware];
 if(process.env.NODE_ENV === 'development'){
   middlewares.push(logger)
 }
-
+//test
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(fetchQuestionsStart)
